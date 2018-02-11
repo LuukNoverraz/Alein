4544b11fab65a2646450b79454db0117610ef51a
Luuk <luuknoverraz@gmail.com>
2018-02-11 21:56:50 +0100

    Een enemy toegevoegd die je bewegingen volgt en een scene die gebruikt kan worden als wachtruimte tussen updates.

---
+++ Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour {
+	public float Speed;
+	private Transform target;
+	Rigidbody2D rigid;
+
+	// Use this for initialization
+	void Start () {
+		target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector2.Distance(transform.position, target.position) < 7)
+		{
+			transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+		}	
+	}
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			//Application.LoadLevel("Scene1");
+		}
+	}
+}
